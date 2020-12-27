35fc952a3490dc0b7583c020b6295ff6c80d1c3d
Joerg Schultze-Lutter <joerg.schultze.lutter@gmail.com>
2020-12-27 23:33:29 +0100

    Removed output generation (will be done by the output generator module)

--- aprs_listener.py
+++ aprs_listener.py
@@ -8,10 +8,9 @@
 #
 
 from input_parser import parsemessage
-from openweathermap_modules import get_daily_weather_from_openweathermapdotorg, parse_daily_weather_from_openweathermapdotorg
 from apscheduler.schedulers.background import BackgroundScheduler
-from airport_data_modules import read_icao_and_iata_data, get_metar_data
 from utility_modules import log_to_stderr
+from output_generator import generate_output_message
 import apscheduler.schedulers.base
 import re
 import sys
@@ -264,60 +263,12 @@ def mycallback(packet):
             # Content parsen
             success, response_parameters = parsemessage(message_text_string, from_string, aprsdotfi_apikey)
             if success:
-                log_to_stderr(response_parameters)
-                what = response_parameters['what']
-                if what == "wx":
-                    latitude = response_parameters['latitude']
-                    longitude = response_parameters['longitude']
-                    units = response_parameters['units']
-                    date_offset = response_parameters['date_offset']
-                    success, myweather, tz_offset, tz = get_daily_weather_from_openweathermapdotorg(latitude=latitude, longitude=longitude, units=units, date_offset=date_offset,openweathermap_api_key=openweathermapdotorg_api_key)
-                    if success:
-                        weather_forecast_array = parse_daily_weather_from_openweathermapdotorg(myweather, units, requested_address, when, when_dt)
-                        SendAprsMessageList(AIS,aprsis_simulate_send,weather_forecast_array,from_string,msg_no_supported)
-                elif what == "help":
-                        SendAprsMessageList(AIS,aprsis_simulate_send,help_text_array,from_string,msg_no_supported)
-                elif what == "metar":
-                        icao_code = response_parameters['icao']
-                        metar_response, found = get_metar_data(icao_code=icao_code)
-                        if found:
-                            SendSingleAprsMessage(AIS,aprsis_simulate_send,metar_response,from_string,msg_no_supported)
-                        else:
-                            print ("metar nicht gefunden; noch Suche anhand Geokoordinaten implementieren")
-                elif what == 'satpass':
-                    satellite = response_parameters['satellite']
-                    latitude = response_parameters['latitude']
-                    longitude = response_parameters['longitude']
-                    altitude = response_parameters['altitude']
-                    when_daytime = response_parameters['when_daytime']
-                    when = response_parameters['when']
-                    print("Satpass")
-                elif what == 'cwop':
-                    latitude = response_parameters['latitude']
-                    longitude = response_parameters['longitude']
-                    cwop_id = response_parameters['cwop_id']
-                    print("CWOP")
-                elif what == 'riseset':
-                    latitude = response_parameters['latitude']
-                    longitude = response_parameters['longitude']
-                    altitude = response_parameters['altitude']
-                    print('Riseset')
-                elif what == "whereis":
-                    latitude = response_parameters['latitude']
-                    longitude = response_parameters['longitude']
-                    altitude = response_parameters['altitude']
-                    when_daytime = response_parameters['when_daytime']
-                    print("Eigene Position ermitteln")
-                elif what == "repeater":
-                    latitude = response_parameters['latitude']
-                    longitude = response_parameters['longitude']
-                    altitude = response_parameters['altitude']
-                    repeater_band = response_parameters['repeater_band']
-                    repeater_mode = response_parameters['repeater_mode']
-                    print("Repeater ermitteln")
+                success, output_message = generate_output_message(response_parameters=response_parameters, openweathermapdotorg_api_key=openweathermapdotorg_api_key)
+                if success:
+                    SendAprsMessageList(AIS, aprsis_simulate_send, output_message, from_string, msg_no_supported)
                 else:
                     # nichts gefunden; Fehlermeldung an User senden
-                    SendSingleAprsMessage(AIS, aprsis_simulate_send, requested_address, from_string, msg_no_supported)
+                    SendSingleAprsMessage(AIS, aprsis_simulate_send, "Fatal error", from_string, msg_no_supported)
                     log_to_stderr(f"Unable to grok packet {packet}")
             else:
                 # nichts gefunden; Fehlermeldung anm User senden
