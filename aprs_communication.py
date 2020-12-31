928e1b0daf41954143632cf34ead1d0df89e1a35
Joerg Schultze-Lutter <joerg.schultze.lutter@gmail.com>
2020-12-31 22:20:06 +0100

    Restructured basic APRS routines

--- aprs_communication.py
+++ aprs_communication.py
@@ -2,12 +2,284 @@
 # Multi-Purpose APRS Daemon: various APRS routines
 # Author: Joerg Schultze-Lutter, 2020
 #
-# Purpose: APRS communication
+# Purpose: APRS communication core functions
 #
 
 import aprslib
-import datetime
-import time
 import logging
+import time
+import re
+from utility_modules import write_number_of_served_packages
+
+# Configuration settings
+mpad_version = "0.01"
+mpad_latitude = "51.8388N"  # 8 chars fixed length, ddmm.mmN, see chapter 6 pg. 23
+mpad_longitude = "008.3266E"  # 9 chars fixed length, dddmm.mmE, see chapter 6 pg. 23
+mpad_alias = "MPAD"  # Identifier for sending data to APRS-IS
+mpad_aprs_tocall = "APRS"  # APRS "TOCALL", see http://aprs.org/aprs11/tocalls.txt. Needs to get its own identifier at a later point in time
 
 
+# Constants, do not change
+aprs_table = "/"  # my symbol table (/=primary \=secondary, or overlay)
+aprs_symbol = "?"  # APRS symbol: Server
+packet_delay_long = 2.0  # packet delay in seconds after sending data to aprs-is
+packet_delay_short = 1.0  # packet delay in seconds after sending data to aprs-is
+
+global number_of_served_packages
+
+# bulletin messages (will be sent every 4 hrs)
+bulletin_texts = {
+    "BLN0": f"{mpad_alias} {mpad_version} APRS WX Bot (Prototype)",
+    "BLN1": f"I have just hatched and am still in alpha test mode. More useful",
+    "BLN2": f"information is going to be added here very soon. Thank you.",
+}
+
+# beacon texts (will be sent every 30 mins)
+beacon_text_array = [
+    f"={mpad_latitude}/{mpad_longitude}{aprs_symbol}{mpad_alias} {mpad_version}",
+    ">My tiny little APRS bot (pre-alpha testing)",
+]
+
+
+def parse_aprs_data(packet_data_dict, item):
+    try:
+        return packet_data_dict.get(item)
+    except Exception:
+        return None
+
+
+def get_aprsis_passcode(call_sign: str = "N0CALL"):
+    """
+    Get the APRS passcode for the given call sign
+    If call sign = 'N0CALL', we will return:
+
+    a) a passcode of -1 (see aprslib.is)
+    b) a flag which tells MPAD that it is only to simulate the send process
+
+    Parameters
+    ==========
+    call_sign: 'str'
+        Call sign string that we want to use for connecting to aprs-is
+
+    Returns
+    =======
+    call_sign: 'str'
+        Same as input parameter; converted to uppercase
+    passcode: 'str'
+        passcode for the call_sign data; -1 if call sign = 'N0CALL'
+    simulate_send_process: 'bool'
+        We will only simulate the send process if this field is True
+    """
+
+    call_sign = call_sign.upper()
+
+    if call_sign == "N0CALL":
+        passcode = "-1"  # read only, see aprslib
+        simulate_send_process = True
+    else:
+        # We got ourselves a real callsign, let's get the passcode
+        passcode = aprslib.passcode(call_sign)
+        simulate_send_process = False
+    return call_sign, passcode, simulate_send_process
+
+
+def send_beacon_and_status_msg(myaprsis: aprslib.inet.IS, simulate_send: bool = True):
+    """
+    Send beacon message list to APRS_IS
+    If 'simulate_send'= True, we still prepare the message but only send it to our log file
+
+    Parameters
+    ==========
+    myaprsis: 'aprslib.inet.IS'
+        Our aprslib object that we will use for the communication part
+    simulate_send: 'bool'
+        If True: Prepare string but only send it to logger
+
+    Returns
+    =======
+    none
+    """
+    global number_of_served_packages
+    logging.debug("Beacon-Intervall erreicht; sende Beacons")
+    for bcn in beacon_text_array:
+        stringtosend = f"{mpad_alias}>{mpad_aprs_tocall}:{bcn}"
+        if not simulate_send:
+            logging.debug(f"echtes Senden: {stringtosend}")
+        else:
+            logging.debug(f"Sende: {stringtosend}")
+    write_number_of_served_packages(number_of_served_packages)
+
+
+def send_bulletin_messages(myaprsis: aprslib.inet.IS, simulate_send: bool = True):
+    """
+    Send bulletin message list to APRS_IS
+    If 'simulate_send'= True, we still prepare the message but only send it to our log file
+
+    Parameters
+    ==========
+    myaprsis: 'aprslib.inet.IS'
+        Our aprslib object that we will use for the communication part
+    simulate_send: 'bool'
+        If True: Prepare string but only send it to logger
+
+    Returns
+    =======
+    none
+    """
+    logging.debug("Bulletin-Intervall erreicht; sende Bulletins")
+    for recipient_id, bln in bulletin_texts.items():
+        stringtosend = f"{mpad_alias}>{mpad_aprs_tocall}::{recipient_id:9}:{bln}"
+        if not simulate_send:
+            logging.debug(f"echtes Senden: {stringtosend}")
+        else:
+            logging.debug(f"Sende: {stringtosend}")
+
+
+def send_ack(
+    myaprsis: aprslib.inet.IS,
+    src_call_sign: str,
+    msg_no: str,
+    simulate_send: bool = True,
+):
+    """
+    Send acknowledgment for received package to APRS_IS if
+    a message number was present
+    If 'simulate_send'= True, we still prepare the message but only send it to our  log file
+
+    Parameters
+    ==========
+    myaprsis: 'aprslib.inet.IS'
+        Our aprslib object that we will use for the communication part
+    src_call_sign: 'str'
+        Call sign of the user that has sent us the message
+    msg_no: 'str'
+        message number from user's request. Can be 'None'. In that case, we don't send a message acknowledgment to the user
+    simulate_send: 'bool'
+        If True: Prepare string but only send it to logger
+
+    Returns
+    =======
+    none
+    """
+
+    if msg_no:
+        logging.debug("Preparing acknowledgment")
+        stringtosend = f"{mpad_alias}>{mpad_aprs_tocall}::{src_call_sign:9}:ack{msg_no}"
+        if not simulate_send:
+            logging.debug(f"echtes Senden: {stringtosend}")
+        else:
+            logging.debug(f"Sende: {stringtosend}")
+
+
+def send_aprs_message_list(
+    myaprsis: aprslib.inet.IS,
+    message_text_array: list,
+    src_call_sign: str,
+    send_with_msg_no: bool,
+    simulate_send: bool = True,
+):
+    """
+    Send a pre-prepared message list to to APRS_IS
+    All packages have a max len of 67 characters
+    If 'simulate_send'= True, we still prepare the message but only send it to our  log file
+
+    Parameters
+    ==========
+    myaprsis: 'aprslib.inet.IS'
+        Our aprslib object that we will use for the communication part
+    message_text_array: 'list'
+        Contains 1..n entries of the content that we want to send to the user
+    src_call_sign: 'str'
+        Call sign of the user that has sent us the message
+    send_with_msg_no: 'bool'
+        If True, each outgoing message will have its own message ID attached to the outgoing content
+        If False, no message ID is added
+    simulate_send: 'bool'
+        If True: Prepare string but only send it to logger
+
+    Returns
+    =======
+    none
+    """
+    global number_of_served_packages
+    for single_message in message_text_array:
+        stringtosend = (
+            f"{mpad_alias}>{mpad_aprs_tocall}::{src_call_sign:9}:{single_message}"
+        )
+        if send_with_msg_no:
+            stringtosend = stringtosend + "}" + f"{number_of_served_packages:05}"
+            number_of_served_packages = number_of_served_packages + 1
+            if number_of_served_packages > 99999:  # max. 5stellig
+                number_of_served_packages = 1
+        if not simulate_send:
+            logging.debug("Echtes Senden")
+        else:
+            logging.debug(stringtosend)
+        time.sleep(packet_delay_short)
+
+
+def send_single_aprs_message(
+    myaprsis: aprslib.inet.IS,
+    message_text: str,
+    src_call_sign: str,
+    send_with_msg_no: bool,
+    simulate_send,
+):
+    """
+    Send a single line of text to APRS_IS
+    Split message up into 1..n chunks of 67 character string if content is too long
+    If 'simulate_send'= True, we still prepare the message but only send it to our log file
+
+    Parameters
+    ==========
+    myaprsis: 'aprslib.inet.IS'
+        Our aprslib object that we will use for the communication part
+    message_text: 'str'
+        Contains 1..n entries of the content that we want to send to the user
+    src_call_sign: 'str'
+        Call sign of the user that has sent us the message
+    send_with_msg_no: 'bool'
+        If True, each outgoing message will have its own message ID attached to the outgoing content
+        If False, no message ID is added
+    simulate_send: 'bool'
+        If True: Prepare string but only send it to logger
+
+    Returns
+    =======
+    none
+    """
+    global number_of_served_packages
+    maxlen = 67  # max. size of APRS message
+    chunks = [message_text[i : i + maxlen] for i in range(0, len(message_text), maxlen)]
+    for chunk in chunks:
+        stringtosend = f"{mpad_alias}>{mpad_aprs_tocall}::{src_call_sign:9}:{chunk}"
+        if send_with_msg_no:
+            stringtosend = stringtosend + "}" + f"{number_of_served_packages:05}"
+            number_of_served_packages = number_of_served_packages + 1
+            if number_of_served_packages > 99999:  # max. 5stellig
+                number_of_served_packages = 1
+        if not simulate_send:
+            logging.debug("Echtes Senden")
+        else:
+            logging.debug(stringtosend)
+        time.sleep(packet_delay_short)
+
+
+def extract_msgno_from_defective_message(message_text: str):
+    msg = msgno = None
+
+    matches = re.search(r"(.*)\{([a-zA-Z0-9]{1,5})\}", message_text, re.IGNORECASE)
+    if matches:
+        try:
+            msg = matches[1]
+            msgno = matches[2]
+        except:
+            msg = message_text
+            msgno = None
+    return msg, msgno
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.DEBUG, format="%(asctime)s %(module)s -%(levelname)s- %(message)s"
+    )
--- aprs_listener.py
+++ aprs_listener.py
@@ -4,12 +4,24 @@
 #
 # Core process
 #
-# Reimplements parts of the WXBOT code from Martin Nile (KI6WJP)
-#
 
 from input_parser import parsemessage
 from apscheduler.schedulers.background import BackgroundScheduler
 from output_generator import generate_output_message
+from utility_modules import (
+    read_program_config,
+    read_number_of_served_packages,
+    write_number_of_served_packages,
+)
+from aprs_communication import (
+    get_aprsis_passcode,
+    parse_aprs_data,
+    send_bulletin_messages,
+    send_beacon_and_status_msg,
+    send_single_aprs_message,
+    send_ack,
+    send_aprs_message_list,
+)
 import apscheduler.schedulers.base
 import re
 import sys
@@ -19,23 +31,15 @@ import aprslib
 import datetime
 import time
 
-# Konfigurationseinstellungen
-myversion = "0.01"
-mylat = "51.8388N"                          # 8 chars fixed length, ddmm.mmN, see chapter 6 pg. 23
-mylon = "008.3266E"                         # 9 chars fixed length, dddmm.mmE, see chapter 6 pg. 23
-mytable = "/"                               # my symbol table (/=primary \=secondary, or overlay)
-mysymbol = "?"                              # APRS symbol: Server
-myalias = "MPAD"                            # Identifier for sending data to APRS-IS
-myaprstocall = "APRS"                       # see http://aprs.org/aprs11/tocalls.txt
-myaprsis_login_callsign="n0call"            # APRS-IS-Login; Passcode wird automatisch errechnet
+# Beacon / Bulletin config settings
+myaprsis_login_callsign = "n0call"  # APRS-IS-Login; Passcode wird automatisch errechnet
 
-myaprs_server_name = "euro.aprs2.net"       # our login server
-myaprs_server_port = 14580                  # server port
-myaprs_server_filter = "g/WXBOT/WXYO"       # server filter criteria for aprs.is
-mycallsigns_to_parse = ['WXBOT', 'WXYO']    # (additional) call sign filter
+myaprs_server_name = "euro.aprs2.net"  # our login server
+myaprs_server_port = 14580  # server port
+myaprs_server_filter = "g/WXBOT/WXYO"  # server filter criteria for aprs.is
+mycallsigns_to_parse = ["WXBOT", "WXYO"]  # (additional) call sign filter
 
-served_packages_filename = "served_packages.mpad"    # Number of served packages
-myicaoiata_filename = "stations.mpad"    # ICAO & IATA data; https://www.aviationweather.gov/docs/metar/stations.txt
+myicaoiata_filename = "stations.mpad"  # ICAO & IATA data; https://www.aviationweather.gov/docs/metar/stations.txt
 
 # will be prepopulated through config file
 aprsdotfi_api_key = None
@@ -43,210 +47,28 @@ openweathermapdotorg_api_key = None
 
 ########################################
 
-# Bulletindaten (Versand alle 4h)
-bulletin_texts = {
-    'BLN0' : f'{myalias} {myversion} APRS WX Bot (Prototype)',
-    'BLN1' : f'I have just hatched and am still in alpha test mode. More useful',
-    'BLN2' : f'information is going to be added here very soon. Thank you.'
-}
-
-# Beacondaten (Versand alle 30 min)
-beacon_text_array = [
-    f'={mylat}/{mylon}{mysymbol}{myalias} {myversion}',
-    '>My tiny little APRS bot (pre-alpha testing)'
-]
 # Send-ID-Counter 1..99999
 number_of_served_packages = 1
 
-# Packet Delays zum Versenden via APRS-IS
-packet_delay_long = 2.0
-packet_delay_short = 1.0
-
-def parse_aprs_data(packet_data_dict, item):
-    try:
-        return packet_data_dict.get(item)
-    except Exception:
-        return None
-
-def read_number_of_served_packages(file_name: str = served_packages_filename):
-    """
-    Read the number of served packages from a file
-
-    If file is not present, we will start with '1'
-
-    Parameters
-    ==========
-    file_name: 'str'
-        Name of the file we are going to read the data from
-
-    Returns
-    =======
-    served_packages: 'int'
-        number of previously served packages (or '1')
-    """
-    served_packages = 1
-    try:
-        with open(f'{file_name}', 'r') as f:
-            if f.mode == 'r':
-                contents = f.read()
-                f.close()
-                served_packages = int(contents)
-    except:
-        served_packages = 1
-        logging.debug(f"Cannot read content from {file_name}")
-    return served_packages
-
-def write_number_of_served_packages(served_packages: int,
-                                    file_name: str = served_packages_filename):
-    """
-    Writes the number of served packages to a file
-
-    Parameters
-    ==========
-    served_packages: 'int'
-        number of previously served packages
-    file_name: 'str'
-        Name of the file we are going to read the data from
-
-    Returns
-    =======
-    None
-    """
-    try:
-        with open(f'{file_name}', 'w') as f:
-            f.write('%d' % sp)
-            f.close()
-    except:
-        logging.debug(f"Cannot write number of served packages to {file_name}")
-
-def get_aprsis_passcode(call_sign: str):
-    """
-    Get the APRS passcode for the given call sign
-    If call sign = 'N0CALL', we will return:
-
-    a) a passcode of -1 (see aprslib.is)
-    b) a flag which tells MPAD that it is only to simulate the send process
-
-    Parameters
-    ==========
-    call_sign: 'str'
-        Call sign string that we want to use for connecting to aprs-is
-
-    Returns
-    =======
-    call_sign: 'str'
-        Same as input parameter; converted to uppercase
-    passcode: 'str'
-        passcode for the call_sign data; -1 if call sign = 'N0CALL'
-    simulate_send_process: 'bool'
-        We will only simulate the send process if this field is True
-    """
-    call_sign = call_sign.upper()
-    if call_sign == "N0CALL":
-        passcode = "-1"         # read only, see aprslib
-        simulate_send_process = True
-    else:
-        passcode = aprslib.passcode(call_sign)
-        simulate_send_process = False
-    return call_sign, passcode, simulate_send_process
-
-def SendBeaconAndStatusMsg(myaprsis,simulate_send):
-    global number_of_served_packages
-    logging.debug("Beacon-Intervall erreicht; sende Beacons")
-    for bcn in beacon_text_array:
-        stringtosend = f"{myalias}>{myaprstocall}:{bcn}"
-        if myaprsis._connected:
-            if not simulate_send:
-                print(f"echtes Senden: {stringtosend}")
-            else:
-                logging.debug(f"Sende: {stringtosend}")
-    write_number_of_served_packages(number_of_served_packages)
-
-def SendBulletinMessages(myaprsis,simulate_send):
-    logging.debug("Bulletin-Intervall erreicht; sende Bulletins")
-    for recipient_id,bln in bulletin_texts.items():
-        stringtosend = f"{myalias}>{myaprstocall}::{recipient_id:9}:{bln}"
-        if myaprsis._connected:
-            if not simulate_send:
-                print(f"echtes Senden: {stringtosend}")
-            else:
-                logging.debug(f"Sende: {stringtosend}")
-
-def SendAck(myaprsis, simulate_send, src_call_sign, msg_no):
-    if msg_no:
-        logging.debug("Sende Ack")
-        stringtosend = f"{myalias}>{myaprstocall}::{src_call_sign:9}:ack{msg_no}"
-        if myaprsis._connected:
-            if not simulate_send:
-                print(f"echtes Senden: {stringtosend}")
-            else:
-                logging.debug(f"Sende: {stringtosend}")
-
-# Senden der bereits aufbereiteten Pakete; es ist sichergestellt, dass keines > 67 Byte ist
-def SendAprsMessageList(aprsis, simulate_send, message_text_array, src_call_sign, send_with_msg_no):
-    global number_of_served_packages
-    for single_message in message_text_array:
-        stringtosend = f"{myalias}>{myaprstocall}::{src_call_sign:9}:{single_message}"
-        if send_with_msg_no:
-            stringtosend = stringtosend+"}"+f"{number_of_served_packages:05}"
-            number_of_served_packages = number_of_served_packages + 1
-            if number_of_served_packages > 99999:  # max. 5stellig
-                number_of_served_packages = 1
-        if aprsis._connected:
-            if not simulate_send:
-                logging.debug("Echtes Senden")
-            else:
-                logging.debug(stringtosend)
-        time.sleep(packet_delay_short)
-
-def SendSingleAprsMessage (aprsis, simulate_send, message_text, src_call_sign, send_with_msg_no):
-    global number_of_served_packages
-    maxlen = 67  # max. size of APRS message
-    chunks = [message_text[i:i + maxlen] for i in range(0, len(message_text), maxlen)]
-    for chunk in chunks:
-        stringtosend = f"{myalias}>{myaprstocall}::{src_call_sign:9}:{chunk}"
-        if send_with_msg_no:
-            stringtosend = stringtosend+"}"+f"{number_of_served_packages:05}"
-            number_of_served_packages = number_of_served_packages + 1
-            if number_of_served_packages > 99999:  # max. 5stellig
-                number_of_served_packages = 1
-        if aprsis._connected:
-            if not simulate_send:
-                logging.debug("Echtes Senden")
-            else:
-                logging.debug(stringtosend)
-        time.sleep(packet_delay_short)
-
-def extract_msgno_from_defective_message(message_text):
-    msg = msgno = None
-
-    matches = re.search(r"(.*)\{([a-zA-Z0-9]{1,5})\}", message_text, re.IGNORECASE)
-    if matches:
-        try:
-            msg = matches[1]
-            msgno = matches[2]
-        except:
-            msg = message_text
-            msgno = None
-    return msg,msgno
 
 # Main-Callback, der alles im Bereich APRS-Kommunikation erledigt
 def mycallback(packet):
-    # Beinhaltet die späteren aufbereiteten Wetternachrichten
-    weather_forecast_array = []
-
     # Basis-APRS-Daten extrahieren
-    addresse_string = parse_aprs_data(packet, 'addresse')
+    addresse_string = parse_aprs_data(packet, "addresse")
     message_text_string = parse_aprs_data(packet, "message_text")
-    msgNo_string = parse_aprs_data(packet, 'msgNo')  # für Acknowledgment der initialen Nachricht
-    from_string = parse_aprs_data(packet, 'from')
+    msgNo_string = parse_aprs_data(
+        packet, "msgNo"
+    )  # für Acknowledgment der initialen Nachricht
+    from_string = parse_aprs_data(packet, "from")
     from_string = from_string.upper()
-    format_string = parse_aprs_data(packet, 'format')
+    format_string = parse_aprs_data(packet, "format")
 
     # Wenn keine MsgNo gefunden, dann prüfen, ob die Message selbst noch eine (ungültige) Message+MessageNo beinhaltet. Falls ja: extrahieren
     # entspricht nicht den Standards, aber offensichtlich kommen solche Pakete ab und zu an
     if not msgNo_string:
-        message_text_string, msgNo_string = extract_msgno_from_defective_message(message_text_string)
+        message_text_string, msgNo_string = extract_msgno_from_defective_message(
+            message_text_string
+        )
 
     # anschließend testen, ob wir jetzt final eine MessageNo erhalten haben
     msg_no_supported = True if msgNo_string else False
@@ -259,20 +81,43 @@ def mycallback(packet):
             # Diese Nachricht ist für uns. Es kann losgehen
             logging.debug(f"received packet: {packet}")
             # ack senden, falls msgNo vorhanden (siehe S. 71ff.)
-            SendAck(AIS,aprsis_simulate_send,from_string, msgNo_string)
+            sned_ack(AIS, aprsis_simulate_send, from_string, msgNo_string)
             # Content parsen
-            success, response_parameters = parsemessage(message_text_string, from_string, aprsdotfi_apikey)
+            success, response_parameters = parsemessage(
+                message_text_string, from_string, aprsdotfi_apikey
+            )
             if success:
-                success, output_message = generate_output_message(response_parameters=response_parameters, openweathermapdotorg_api_key=openweathermapdotorg_api_key)
+                success, output_message = generate_output_message(
+                    response_parameters=response_parameters,
+                    openweathermapdotorg_api_key=openweathermapdotorg_api_key,
+                )
                 if success:
-                    SendAprsMessageList(AIS, aprsis_simulate_send, output_message, from_string, msg_no_supported)
+                    send_aprs_message_list(
+                        AIS,
+                        aprsis_simulate_send,
+                        output_message,
+                        from_string,
+                        msg_no_supported,
+                    )
                 else:
                     # nichts gefunden; Fehlermeldung an User senden
-                    SendSingleAprsMessage(AIS, aprsis_simulate_send, "Fatal error", from_string, msg_no_supported)
+                    send_single_aprs_message(
+                        AIS,
+                        aprsis_simulate_send,
+                        "Fatal error",
+                        from_string,
+                        msg_no_supported,
+                    )
                     loggging.debug(f"Unable to grok packet {packet}")
             else:
                 # nichts gefunden; Fehlermeldung anm User senden
-                SendSingleAprsMessage(AIS,aprsis_simulate_send, requested_address,from_string,msg_no_supported)
+                send_single_aprs_message(
+                    AIS,
+                    aprsis_simulate_send,
+                    requested_address,
+                    from_string,
+                    msg_no_supported,
+                )
                 logging.debug(f"Unable to grok packet {packet}")
 
 
@@ -280,8 +125,12 @@ def mycallback(packet):
 # main
 #
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(module)s -%(levelname)s - %(message)s')
-aprsis_callsign, aprsis_passcode, aprsis_simulate_send = get_aprsis_passcode(myaprsis_login_callsign)
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(module)s -%(levelname)s - %(message)s"
+)
+aprsis_callsign, aprsis_passcode, aprsis_simulate_send = get_aprsis_passcode(
+    myaprsis_login_callsign
+)
 number_of_served_packages = read_number_of_served_packages()
 read_icao_and_iata_data()
 success, aprsdotfi_api_key, openweathermapdotorg_api_key = read_program_config()
@@ -291,31 +140,47 @@ if not success:
 
 try:
     while True:
-        reconnect_timestamp = status_timestamp = beacon_timestamp = datetime.datetime.now()
+        reconnect_timestamp = (
+            status_timestamp
+        ) = beacon_timestamp = datetime.datetime.now()
         # Callsign und Passcode setzen
-        AIS = aprslib.IS(aprsis_callsign,aprsis_passcode)
+        AIS = aprslib.IS(aprsis_callsign, aprsis_passcode)
 
         # Filterport und zugehörigen Filter setzen
         AIS.set_server(myaprs_server_name, myaprs_server_port)
         AIS.set_filter(myaprs_server_filter)
 
-        logging.debug(f"Verbindung herstellen: Server={myaprs_server_name}, port={myaprs_server_port}, filter={myaprs_server_filter}, APRS-IS User: {aprsis_callsign}, APRS-IS Passcode: {aprsis_passcode}")
+        logging.debug(
+            f"Verbindung herstellen: Server={myaprs_server_name}, port={myaprs_server_port}, filter={myaprs_server_filter}, APRS-IS User: {aprsis_callsign}, APRS-IS Passcode: {aprsis_passcode}"
+        )
         AIS.connect(blocking=True)
         if AIS._connected == True:
             logging.debug("Verbindung aufgebaut")
 
             # Initiales Beacon versenden
             logging.debug("Initiales Beacon nach Verbindungsaufbau senden")
-            SendBeaconAndStatusMsg(AIS,aprsis_simulate_send)
+            send_beacon_and_status_msg(AIS, aprsis_simulate_send)
 
-           #Scheduler einrichten und starten
+            # Scheduler einrichten und starten
             aprs_scheduler = BackgroundScheduler()
-            aprs_scheduler.add_job(SendBeaconAndStatusMsg, 'interval', id='bulletin', minutes=30,args=[AIS,aprsis_simulate_send])
-            aprs_scheduler.add_job(SendBulletinMessages, 'interval', id='status', minutes=4*60, args=[AIS, aprsis_simulate_send])
+            aprs_scheduler.add_job(
+                send_beacon_and_status_msg,
+                "interval",
+                id="bulletin",
+                minutes=30,
+                args=[AIS, aprsis_simulate_send],
+            )
+            aprs_scheduler.add_job(
+                send_bulletin_messages,
+                "interval",
+                id="status",
+                minutes=4 * 60,
+                args=[AIS, aprsis_simulate_send],
+            )
             aprs_scheduler.start()
 
             logging.debug("Starte Callback-Consumer")
-            AIS.consumer(mycallback, blocking=True, immortal=True,raw=False)
+            AIS.consumer(mycallback, blocking=True, immortal=True, raw=False)
 
             logging.debug("Callback verlassen")
             # Scheduler zuerst stoppen, leeren und dann herunterfahren
