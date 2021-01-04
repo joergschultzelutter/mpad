fc8b04f2283b5cf068d1b5c6b7c79f52378c23b1
Joerg Schultze-Lutter <joerg.schultze.lutter@gmail.com>
2021-01-04 18:59:17 +0100

    changed logging

--- src/parser_test.py
+++ src/parser_test.py
@@ -1,46 +1,40 @@
-import unittest
 from utility_modules import read_program_config
 from input_parser import parse_input_message
 from output_generator import generate_output_message
 import logging
 
 
-class MyTestCase(unittest.TestCase):
-    def test123(self):
-        success, aprsdotfi_api_key, openweathermapdotorg_api_key = read_program_config()
-        assert success == True
+def testcall():
+    success, aprsdotfi_api_key, openweathermapdotorg_api_key = read_program_config()
+    assert success == True
 
-        # message_text = APRS-Message excluding any message IDs etc
-        message_text = "Current"
-        # from_callsign = Sender's callsign
-        from_callsign = "df1jsl-1"
+    # message_text = APRS-Message excluding any message IDs etc
+    message_text = "Full"
+    # from_callsign = Sender's callsign
+    from_callsign = "df1jsl-1"
 
-        logging.debug(
-            f"parsing message '{message_text}' for callsign '{from_callsign}'"
-        )
+    logger.debug(
+        f"parsing message '{message_text}' for callsign '{from_callsign}'"
+    )
 
-        success, response_parameters = parse_input_message(
-            message_text, from_callsign, aprsdotfi_api_key
-        )
-        logging.debug("Parser:")
-        logging.debug(response_parameters)
-        if success:
-            logging.debug("Response:")
-            logging.debug(
-                generate_output_message(
-                    response_parameters, openweathermapdotorg_api_key
-                )
+    success, response_parameters = parse_input_message(
+        message_text, from_callsign, aprsdotfi_api_key
+    )
+    logger.debug("Parser:")
+    logger.debug(response_parameters)
+    if success:
+        logger.debug("Response:")
+        logger.debug(
+            generate_output_message(
+                response_parameters, openweathermapdotorg_api_key
             )
-        else:
-            logging.debug(response_parameters)
+        )
+    else:
+        logger.debug(response_parameters)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.DEBUG, format="%(asctime)s %(module)s -%(levelname)s- %(message)s"
-    )
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(module)s -%(levelname)s- %(message)s')
     logger = logging.getLogger(__name__)
 
-    logger.debug("Start of program")
-    unittest.main()
-    logger.debug("End of program")
+    testcall()
