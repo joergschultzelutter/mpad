f4c8499a329958df9272ce33801735db20b753cf
Joerg Schultze-Lutter <joerg.schultze.lutter@gmail.com>
2021-01-25 23:28:12 +0100

    Add position's age data to 'whereis' requests

--- src/output_generator.py
+++ src/output_generator.py
@@ -580,6 +580,14 @@ def generate_output_message_whereis(response_parameters: dict):
         List, containing the message text(s) that we will send to the user
         This is plain text list without APRS message ID's
     """
+
+    # This variable will be set to true if lat/lon differ from
+    # user's lat/lon, meaning that these callsigns might represent
+    # different identities (based on their positions)
+    # False = we run the 'whereami' mode
+    # True = we run the 'whereis' mode
+    _whereis_mode = False
+
     latitude = response_parameters["latitude"]
     longitude = response_parameters["longitude"]
     altitude = response_parameters["altitude"]
@@ -587,6 +595,9 @@ def generate_output_message_whereis(response_parameters: dict):
     users_latitude = response_parameters["users_latitude"]
     users_longitude = response_parameters["users_longitude"]
     units = response_parameters["units"]
+    lasttime = response_parameters["lasttime"]
+    if not isinstance(lasttime, datetime.datetime):
+        lasttime = datetime.datetime.min
 
     # all of the following data was reverse-lookup'ed and can be 'None'
     city = response_parameters["city"]
@@ -632,6 +643,12 @@ def generate_output_message_whereis(response_parameters: dict):
     # calculate distance, heading and bearing if message call sign position
     # differs from our own call sign's position
     if latitude != users_latitude and longitude != users_longitude:
+
+        # We have different identities and switch from "whereami" mode
+        # to "whereis" mode
+        _whereis_mode = True
+
+        # Calculate distance, bearing and heading
         distance, bearing, heading = haversine(
             latitude1=latitude,
             longitude1=longitude,
@@ -717,6 +734,40 @@ def generate_output_message_whereis(response_parameters: dict):
         output_list = make_pretty_aprs_messages(
             message_to_add=human_readable_address, destination_list=output_list
         )
+
+    if _whereis_mode:
+        human_readable_address = None
+        # Check if we have some "lasttime" information that we can provide the user with
+        if lasttime is not datetime.datetime.min:
+            # Determine time difference between utc'ed systime and timestamp from aprs.fi
+            # (which is always in utc tz)
+            current_ts = datetime.datetime.utcnow()
+            last_heard = current_ts - lasttime
+
+            # calculate mins, hrs, days, yrs
+
+            _days, _secs = last_heard.days, last_heard.seconds
+            _hrs = _days * 24 + _secs // 3600
+            _min = (_secs % 3600) // 60
+            _secs = _secs % 60
+            _years = int(_days / 365)
+
+            # Now let's check what we have and add it as PosAge to our message
+            if _years != 0:
+                human_readable_address = f"PosAge {_years} yrs {_days % 365} days"
+            elif _days != 0:
+                human_readable_address = f"PosAge {_days} days {_hrs % 24} hrs"
+            elif _hrs != 0:
+                human_readable_address = f"PosAge {_hrs} hrs {_min % 60} mins"
+            elif _min != 0:
+                human_readable_address = f"PosAge {_min} min {_secs % 60} secs"
+            elif _secs != 0:
+                human_readable_address = f"PosAge {_secs}sec"
+
+            if human_readable_address:
+                output_list = make_pretty_aprs_messages(
+                    message_to_add=human_readable_address, destination_list=output_list
+                )
 
     success = True
     return success, output_list
