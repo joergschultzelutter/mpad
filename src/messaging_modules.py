f941be14fae84e1770f71b8625f7c2f48608b63c
Joerg Schultze-Lutter <76180229+joergschultzelutter@users.noreply.github.com>
2024-05-29 14:05:57 +0200

    Initial upload
    Messaging  module for program-related errors

---
+++ src/messaging_modules.py
@@ -0,0 +1,92 @@
+#
+# Multi-Purpose APRS Daemon
+# Author: Joerg Schultze-Lutter, 2024
+# Module: Apprise messaging
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+import logging
+import apprise
+from utility_modules import check_if_file_exists
+
+# Set up the global logger variable
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s %(module)s -%(levelname)s- %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+
+def generate_apprise_message(
+        message_header: str,
+        message_body: str,
+        apprise_config_file: str,
+):
+    """
+    Generates Apprise messages and triggers transmission to the user
+
+    Parameters
+    ==========
+    message_header : 'str'
+        The message header that we want to send to the user
+    message_body : 'str'
+        The message body that we want to send to the user
+    apprise_config_file: 'str'
+        Apprise Yaml configuration file
+
+    Returns
+    =======
+    success: 'bool'
+        True if successful
+    """
+
+    # predefine the output value
+    success = False
+
+    logger.debug(msg="Starting Apprise message processing")
+
+    if not check_if_file_exists(apprise_config_file):
+        logger.error(
+            msg=f"Apprise config file {apprise_config_file} does not exist; aborting"
+        )
+        return False
+
+    # Create the Apprise instance
+    apobj = apprise.Apprise()
+
+    # Create an Config instance
+    config = apprise.AppriseConfig()
+
+    # Add a configuration source:
+    config.add(apprise_config_file)
+
+    # Make sure to add our config into our apprise object
+    apobj.add(config)
+
+    # Send the notification
+    apobj.notify(
+        body=message_body,
+        title=message_header,
+        tag="all",
+        notify_type=apprise.NotifyType.FAILURE,
+    )
+
+    success = True
+
+    logger.debug(msg="Finished Apprise message processing")
+    return success
+
+
+if __name__ == "__main__":
+    pass
